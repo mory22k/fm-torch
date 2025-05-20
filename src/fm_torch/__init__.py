74ef51c615b2bc7e895dcaf950b6bb18bac1feda
mory22k <mory22k@dc.tohoku.ac.jp>
2025-05-20 16:43:56 +0900

    style(src): change the filename of main code

--- src/fm_torch/__init__.py
+++ src/fm_torch/__init__.py
@@ -1,5 +1,5 @@
 """fm_torch: A PyTorch implementation of Factorization Machines (FM) for efficient learning of interactions between features."""
 
-from .fm import SecondOrderFactorizationMachine
+from ._fm import SecondOrderFactorizationMachine
 
 __all__ = ["SecondOrderFactorizationMachine"]
